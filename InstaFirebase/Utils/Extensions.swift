1846e8d9d1758eaa0289167ad03316b88776189c
Mahmoud Salah <modsah53@yahoo.com>
2019-09-16 07:05:55 +0200

    1- build signUp Screen using code only. 2- Add Firebase pod

---
+++ InstaFirebase/Utils/Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Extensions.swift
+//  InstaFirebase
+//
+//  Created by MhMuD SalAh!! on 9/14/19.
+//  Copyright © 2019 Mahmoud Salah. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor{
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: red/255, blue: red/255, alpha: 1)
+    }
+}
